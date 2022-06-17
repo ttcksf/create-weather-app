937dd4c8504d179899519946eba5cbb3e7a2d207
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-06-18 02:40:14 +0900

    create Footer.js 20220617

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer style={style.footer}>
+      <p>copyright @weather-api-app</p>
+    </footer>
+  );
+};
+
+const style = {
+  footer: {
+    background: "#c4c4c4",
+    textAlign: "center",
+    padding: "5px 0",
+    fontSize: "10px",
+  },
 };
 
 export default Footer;
