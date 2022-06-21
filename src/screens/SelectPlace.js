cd35e1e1697f033cb8644fd7b2cf1d6704803f8d
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-06-21 19:24:26 +0900

    create SelectPlace.js 20220621

--- src/screens/SelectPlace.js
+++ src/screens/SelectPlace.js
@@ -10,21 +10,57 @@ const SelectPlace = () => {
   return (
     <>
       <Header />
-      <h1>あなたの場所は？</h1>
-      <div>
-        <select>
-          {Prefectures.map((prefecture, id) => {
-            return <option value={prefecture.value}>{prefecture.name}</option>;
-          })}
-        </select>
-      </div>
-      <Link to={`/today-weather`}>送信</Link>
-      <p>
-        <img src={ClickMark} alt="" />
-      </p>
+      <main>
+        <div style={style.SelectContainer}>
+          <div style={style.SelectWrapper}>
+            <h1 style={style.SelectTitle}>あなたの場所は？</h1>
+            <div style={style.SelecAria}>
+              <select style={style.SelectPullDown}>
+                {Prefectures.map((prefecture, id) => {
+                  return (
+                    <option value={prefecture.value}>{prefecture.name}</option>
+                  );
+                })}
+              </select>
+              <Link className="btn" to={`/today-weather`}>
+                送信
+              </Link>
+            </div>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );
+};
+
+const style = {
+  SelectContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  SelectWrapper: {
+    width: "80%",
+  },
+  SelectTitle: {
+    fontSize: "20px",
+    fontWeight: "400",
+    textAlign: "center",
+    marginBottom: "20px",
+  },
+  SelecAria: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  SelectPullDown: {
+    width: "300px",
+    height: "30px",
+    marginRight: "30px",
+  },
 };
 
 export default SelectPlace;
