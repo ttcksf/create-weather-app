0a878de60a09df9fce10a82149fb0f2eda22e5e7
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-07-02 02:38:27 +0900

    create InputFormText.js InputFormImage.js

--- src/components/InputFormImage.js
+++ src/components/InputFormImage.js
@@ -1,7 +1,68 @@
 import React from "react";
+import "../index.css";
+import ClickMark from "../img/clickmark.png";
 
 const InputFormImage = () => {
-  return <div>InputFormImage</div>;
+  return (
+    <div>
+      <div style={style.SelectWrapper}>
+        <h2 style={style.SelectImageTitle}>あなたの今日の気分は？</h2>
+        <div style={style.InputAria}>
+          <input
+            type="radio"
+            name="sizeSelect"
+            value="VeryGood"
+            id="VeryGood"
+          />
+          <label style={style.SelectImageLabel} for="VeryGood">
+            大変良い
+          </label>
+          <input type="radio" name="sizeSelect" value="Good" id="Good" />
+          <label style={style.SelectImageLabel} for="Good">
+            良い
+          </label>
+          <input type="radio" name="sizeSelect" value="Bad" id="Bad" />
+          <label style={style.SelectImageLabel} for="Bad">
+            不調
+          </label>
+          <input type="radio" name="sizeSelect" value="VeryBad" id="VeryBad" />
+          <label style={style.SelectImageLabel} for="VeryBad">
+            絶不調
+          </label>
+        </div>
+        <img style={style.ClickMark} src={ClickMark} alt="" />
+      </div>
+    </div>
+  );
 };
 
+const style = {
+  SelectWrapper: {
+    width: "80%",
+    margin: "50px auto",
+  },
+  SelectImageTitle: {
+    fontSize: "32px",
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: "20px",
+  },
+  InputAria: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  SelectImageLabel: {
+    border: "1px solid #333",
+    padding: "5px",
+    textAlign: "center",
+    fontSize: "18px",
+  },
+  ClickMark: {
+    width: "100px",
+    height: "auto",
+    float: "right",
+  },
+};
 export default InputFormImage;
--- src/components/InputFormText.js
+++ src/components/InputFormText.js
@@ -1,7 +1,49 @@
 import React from "react";
 
 const InputFormText = () => {
-  return <div>InputFormText</div>;
+  return (
+    <div>
+      <div style={style.InputWrapper}>
+        <h1 style={style.InputFormTitle}>あなたの今日の大事なご予定は？</h1>
+        <div style={style.InputAria}>
+          <form style={style.FormArea}>
+            <input style={style.InputBox} type="text" />
+          </form>
+          <button type="submit" className="btn">
+            送信
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+const style = {
+  InputWrapper: {
+    width: "80%",
+    margin: "50px auto",
+  },
+  InputFormTitle: {
+    fontSize: "32px",
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: "20px",
+  },
+  InputAria: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  FormArea: {
+    width: "80%",
+    margin: "10px auto",
+  },
+  InputBox: {
+    width: "100%",
+    height: "50px",
+    marginRight: "30px",
+    border: "1px solid #333",
+  },
 };
 
 export default InputFormText;
