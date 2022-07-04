6c59894b66d95b433da5da02f7a73439ace578e1
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-07-04 23:48:32 +0900

    create grand_level in Table.js

---
+++ src/components/HumanIcons/Bad.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BadImg from "../../img/bad.png";
+
+const Bad = () => {
+  return (
+    <div>
+      <img src={BadImg} alt="" />
+    </div>
+  );
+};
+
+export default Bad;
---
+++ src/components/HumanIcons/Good.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GoodImg from "../../img/good.png";
+
+const Good = () => {
+  return (
+    <div>
+      <img src={GoodImg} alt="" />
+    </div>
+  );
+};
+
+export default Good;
